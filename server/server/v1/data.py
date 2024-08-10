92abcd4e6f66f6eab562c76d3ee4f66761c4a8b3
Jaeyong Sung <jaeyong0201@gmail.com>
2024-08-11 06:59:26 +0900

    update

--- server/server/v1/data.py
+++ server/server/v1/data.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from aiofile import async_open
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/auth")
+router = APIRouter(prefix="/data")
 
 
-@router.get("/data")
+@router.get("/")
 async def get_data(type: str, time: str):
     filename = f"{type}-{time}.csv"
     path_ = Path(__file__).parent / "data" / filename
